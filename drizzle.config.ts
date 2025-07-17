ab0e1689a3cb27e002be76a7b7ed75f502e5243b
Rutvikraj Champavat <rrchampavat1110@gmail.com>
2025-07-17 12:49:18 +0530

    FIX: Replaced supabase db with neon

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,25 +1,19 @@
 import "dotenv/config";
 import type { Config } from "drizzle-kit";
-import {
-  DB_CONNECTION_STRING,
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_USER
-} from "./src/constants/envVars";
+import { DB_CONNECTION_STRING } from "./src/constants/envVars";
 
 export default {
   schema: "./src/db/schemas/*",
   out: "./drizzle",
   schemaFilter: ["ecommerce-schema"],
   driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+    // host: DB_HOST,
+    // port: DB_PORT,
+    // user: DB_USER,
+    // password: DB_PASSWORD,
+    // database: DB_NAME,
     connectionString: DB_CONNECTION_STRING,
     ssl: false
   }
